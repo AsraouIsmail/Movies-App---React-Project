5fff54923082497baf1d4337746292f26dcff139
Asraou Ismail <asraou.ismail@gmail.com>
2020-12-25 23:38:55 +0100

    the end of project

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popula
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=7c6452a7ceee3e54289a9c6ed83a6af1";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=7c6452a7ceee3e54289a9c6ed83a6af1&query=";
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm] = useState("");
 
   useEffect(() => {
     fetch(FEATURED_API)
@@ -20,13 +21,51 @@ function App() {
     });
     
   }, []);
+
+
+ 
+  const handleOnSubmit = (e) =>{
+
+    e.preventDefault();
+    if(searchTerm) {
+
+
+       fetch(SEARCH_API + searchTerm)
+    .then((res) => res.json())
+    .then((data) => {
+    setMovies(data.results);
+  });
+
+  setsearchTerm('');
+
+    }
+
+     
+};
+
+ const handleOnChange = (e) => {
+    setsearchTerm(e.target.value);
+  }
+
   
   return (
+    <>
+
+    <header>
+      <form onSubmit={handleOnSubmit}>
+
+       <input type="search"  className="search" placeholder="Search your favorit movie here......" value={searchTerm} onChange={handleOnChange} 
+
+       />
+
+      </form>
+        
+      </header>
     <div className="movie-container">
 
-      {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
-      
+      { movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
     </div>
+    </>
   );
 }
 
